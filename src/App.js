b893309bde46a6971f616afc5a834797ce4d588b
Olu <theoafactor@gmail.com>
2022-04-20 13:05:26 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
+import Menu from "./Components/Menu/Menu.js"
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
